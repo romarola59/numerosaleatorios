d1da49132d2e855694f07e990a3f544c0ecc0d1a
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-09 03:54:38 -0300

    Agregar archivos de proyecto.

---
+++ numeros aleatorios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace numeros_aleatorios
+{
+    publicclass Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
