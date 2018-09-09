c5e1dfd0c6ef90739690f6377b4c710084cb5151
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-09 04:08:25 -0300

    numeros aleatorios

--- numeros aleatorios/Program.cs
+++ numeros aleatorios/Program.cs
@@ -2,11 +2,18 @@
 
 namespace numeros_aleatorios
 {
-    publicclass Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Random r = new Random();
+            int aleatorio = r.Next(1, 11);
+            Console.WriteLine("un numero entero entre 1 y 10 es:{0} ", aleatorio);
+            int aleatorio2 = r.Next(10, 21);
+            Console.WriteLine("Otro entre 10 y 20 es: {0}", aleatorio2);
+
+
+            Console.ReadKey();
         }
     }
 }
